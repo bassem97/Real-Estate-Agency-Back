e61a7fe6179401261ad94d1b25da74b4188a66e5
bassem jadoui <36534106+bassem97@users.noreply.github.com>
2020-02-15 02:24:14 +0100

    image upload

--- src/main/java/com/wecode/realestateagency/Controllers/LocalController.java
+++ src/main/java/com/wecode/realestateagency/Controllers/LocalController.java
@@ -4,9 +4,16 @@ import com.wecode.realestateagency.Models.Local;
 import com.wecode.realestateagency.Services.Client.ClientServiceImplement;
 import com.wecode.realestateagency.Services.Local.LocalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -44,6 +51,15 @@ public class LocalController {
         return localService.findByAdress(address);
     }
 
+    @RequestMapping(value="upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE )
+    public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File("C:\\Users\\Bassem's PC\\Desktop\\DSI3\\stage perfectionnement\\real estate agency back\\src\\main\\resources\\Images"+ file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
+        return new ResponseEntity<>("File is uploaded ", HttpStatus.OK) ;
+    }
 
 
 
